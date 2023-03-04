c9467932f88332cf7d444cb6847813877f5413a1
caicost <caiocrodrigues99@hotmail.com>
2023-03-04 17:57:04 -0300

    Projeto Smart Tv

--- Metodos/src/Usuario.java
+++ Metodos/src/Usuario.java
@@ -8,22 +8,22 @@ public class Usuario {
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
 
-        System.out.println("Canal Atual ? " + smartTv.canal);
+            System.out.println("Canal Atual ? " + smartTv.canal);
 
         smartTv.mudarCanal(13);
 
-        System.out.println("Volume Atual ? " + smartTv.volume);
+            System.out.println("Volume Atual ? " + smartTv.volume);
 
 
-        System.out.println("TV Ligada ? " + smartTv.ligada);
-        System.out.println("Canal Atual ? " + smartTv.canal);
-        System.out.println("Volume Atual ? " + smartTv.volume);
+            System.out.println("TV Ligada ? " + smartTv.ligada);
+            System.out.println("Canal Atual ? " + smartTv.canal);
+            System.out.println("Volume Atual ? " + smartTv.volume);
 
         smartTv.ligar();
-        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+            System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
 
         smartTv.desligar();
-        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+            System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
 
     }
 
