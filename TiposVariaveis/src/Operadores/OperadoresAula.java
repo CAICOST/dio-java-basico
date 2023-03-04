ae628a62011ef1933c370b2fdd1731ec322e8526
caicost <caiocrodrigues99@hotmail.com>
2023-03-04 17:54:51 -0300

    Projeto Smart Tv

---
+++ TiposVariaveis/src/Operadores/OperadoresAula.java
@@ -0,0 +1,25 @@
+package Operadores;
+
+public class OperadoresAula {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a numeDois? " + simNao);
+
+        boolean simNo = numero1 != numero2;
+
+        System.out.println("numeroUm é igual a numeDois? " + simNo);
+
+
+
+
+
+    }
+
+
+
+}
+
