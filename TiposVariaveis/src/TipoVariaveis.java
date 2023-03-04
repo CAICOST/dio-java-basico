924dd554318fed488356848b6dd99ad5f151cfed
caicost <caiocrodrigues99@hotmail.com>
2023-03-04 09:02:42 -0300

    Tipos Variaveis

---
+++ TiposVariaveis/src/TipoVariaveis.java
@@ -0,0 +1,21 @@
+public class TipoVariaveis {
+
+    public static void main(String[] args) {
+        int numeroCurto = 1;
+
+        numeroCurto = 30;
+
+        int numeroNormal = numeroCurto;
+        int numeroCurto2 = numeroNormal;
+
+
+
+            System.out.println (numeroCurto + numeroCurto2);
+
+
+
+
+    }
+
+}
+
